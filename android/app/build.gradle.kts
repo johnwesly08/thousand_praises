8db4da3090f18fe280cf81dbc4ad5799befb9365
John Wesly P D <112474505+johnwesly08@users.noreply.github.com>
2026-04-29 02:23:26 +0530

    Update Android build configuration for SDK versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,34 +7,20 @@ plugins {
 
 android {
     namespace = "com.example.thousand_praises"
-    compileSdk = 36
-    ndkVersion = "27.0.12077973"
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+    compileSdk = 34
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.thousand_praises"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 34
+
         versionCode = 1
         versionName = "1.0.0"
-}
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+        getByName("release") {
+            isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
